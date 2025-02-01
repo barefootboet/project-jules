3eca44134f472124140aa204322484f62f711eb4
barefootboet <stuarthurwitz@gmail.com>
2025-02-01 20:41:44 +0000

    robyn test 2

--- app/sales-people/[id]/page.tsx
+++ app/sales-people/[id]/page.tsx
@@ -1,5 +1,17 @@
+import { Metadata } from 'next'
 import SalesPersonDetails from '@/app/components/SalesPersonDetails';
 
-export default function SalesPersonPage({ params }: { params: { id: string } }) {
+interface PageProps {
+  params: {
+    id: string;
+  };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+export const metadata: Metadata = {
+  title: 'Sales Person Details',
+}
+
+export default function SalesPersonPage({ params }: PageProps) {
   return <SalesPersonDetails id={params.id} />;
 } 
